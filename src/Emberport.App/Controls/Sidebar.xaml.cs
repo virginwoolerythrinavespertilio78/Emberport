989b48447e411a857714cd2381babf52e812e6f9
Hojjat Jahanpour <h.jahanpoor1384@gmail.com>
2026-07-24 13:52:02 +0330

    feat: add navigation sidebar with sectioned items and active indicator

---
+++ src/Emberport.App/Controls/Sidebar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Emberport.Controls;
+
+public partial class Sidebar : UserControl
+{
+    public Sidebar()
+    {
+        InitializeComponent();
+    }
+}
