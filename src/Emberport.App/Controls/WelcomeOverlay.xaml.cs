1e0ca0b001951d1f3f9aa2c9458166df11f4ca0d
Hojjat Jahanpour <h.jahanpoor1384@gmail.com>
2026-07-25 12:38:49 +0330

    feat: add an animated welcome overlay with a github support call to action

---
+++ src/Emberport.App/Controls/WelcomeOverlay.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace Emberport.Controls;
+
+public partial class WelcomeOverlay : UserControl
+{
+    private const string RepositoryUrl = "https://github.com/hojjatjh";
+
+    private Window? _host;
+
+    public WelcomeOverlay()
+    {
+        InitializeComponent();
+        Loaded += OnLoaded;
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        _host = Window.GetWindow(this);
+
+        if (_host is not null)
+        {
+            _host.PreviewKeyDown += OnHostKeyDown;
+        }
+
+        ((Storyboard)Resources["ShowStoryboard"]).Begin(this);
+    }
+
+    private void OnHostKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key is Key.Escape or Key.Enter)
+        {
+            Dismiss();
+        }
+    }
+
+    private void OnStarClick(object sender, RoutedEventArgs e)
+    {
+        // UseShellExecute lets Windows resolve the default browser.
+        Process.Start(new ProcessStartInfo(RepositoryUrl) { UseShellExecute = true });
+        Dismiss();
+    }
+
+    private void OnContinueClick(object sender, RoutedEventArgs e) => Dismiss();
+
+    private void Dismiss()
+    {
+        if (_host is not null)
+        {
+            _host.PreviewKeyDown -= OnHostKeyDown;
+            _host = null;
+        }
+
+        ((Storyboard)Resources["HideStoryboard"]).Begin(this);
+    }
+
+    private void OnHideCompleted(object? sender, EventArgs e) =>
+        Visibility = Visibility.Collapsed;
+}
